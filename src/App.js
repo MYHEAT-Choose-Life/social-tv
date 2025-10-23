a0e53e4481a5101d36c8fd7da68389a1264a963c
unknown <emmamukwa@gmail.com>
2025-10-23 16:17:18 +0200

    css files added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import TopBar from './components/TopBar';
 import Navbar from './components/Navbar';
+import BreakingNews from './components/BreakingNews';
 
 function App() {
   return (
     <div>
       <TopBar />
       <Navbar/>
+      <BreakingNews/>
       {/* Add Navbar and BreakingNews next */}
     </div>
   );
